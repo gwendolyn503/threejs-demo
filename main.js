02ce73e471842c1b6f3dd1b97eb8ad8619317eb3
林格温 <wb.wulingshan01@mesg.corp.netease.com>
2024-12-05 18:01:43 +0800

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: wu_lingshan
+ * @Date: 2024-12-05 17:54:25
+ * @LastEditors: wu_lingshan
+ * @LastEditTime: 2024-12-05 17:54:28
+ * @Description: 请填写简介
+ */
+import * as THREE from 'three';
