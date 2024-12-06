ac9cfaf0b9b71d3ec20343e90f725698262beb60
林格温 <wb.wulingshan01@mesg.corp.netease.com>
2024-12-06 18:25:02 +0800

    basic learning1

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 /*
  * @Author: wu_lingshan
- * @Date: 2024-12-05 17:54:25
+ * @Date: 2024-12-06 17:04:59
  * @LastEditors: wu_lingshan
- * @LastEditTime: 2024-12-05 17:54:28
- * @Description: 请填写简介
+ * @LastEditTime: 2024-12-06 17:16:56
+ * @Description: Loading 3D models
  */
 import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const loader = new GLTFLoader();
+
+loader.load('public/scene.gltf', function (gltf) {
+
+    scene.add(gltf.scene);
+
+}, undefined, function (error) {
+
+    console.error(error);
+
+});
+
+renderer.render(scene, camera);
